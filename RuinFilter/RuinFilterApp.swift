698ff2cb32c8c8c975590d569465ae3fc219582b
snarfnet <82538502+snarfnet@users.noreply.github.com>
2026-05-06 22:41:32 +0900

    Make RuinFilter UI scarier and prepare release

--- RuinFilter/RuinFilterApp.swift
+++ RuinFilter/RuinFilterApp.swift
@@ -1,10 +1,24 @@
 import SwiftUI
+import GoogleMobileAds
+import AppTrackingTransparency
 
 @main
 struct RuinFilterApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear {
+                    if AppRuntime.showsAds {
+                        GADMobileAds.sharedInstance().start(completionHandler: nil)
+                    }
+                }
+                .onReceive(NotificationCenter.default.publisher(for: UIApplication.didBecomeActiveNotification)) { _ in
+                    if AppRuntime.showsAds {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                            ATTrackingManager.requestTrackingAuthorization { _ in }
+                        }
+                    }
+                }
         }
     }
 }
